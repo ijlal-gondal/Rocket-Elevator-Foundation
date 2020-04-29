1e1f58a662199c90dccc80d10da4f2a48c5e1875
Ijlal Gondal <ijlal.gondal@ontariotechu.net>
2020-04-29 12:58:48 -0400

    again

--- lib/speaker_identification/create_enrollment_api.rb
+++ lib/speaker_identification/create_enrollment_api.rb
@@ -1,4 +1,5 @@
 require 'net/http'
+require 'json'
 
 module SpeakerIdentification
 class Enrollment
@@ -7,11 +8,11 @@ def self.test
     puts "beginning test"
 
 
-identificationProfileId = "35f500b0-40c1-4160-bf83-cf2a7f7e49e7"
+identificationProfileId = "492159e5-2562-48c0-a354-481ef6341f8d"
 uri = URI("https://westus.api.cognitive.microsoft.com/spid/v1.0/identificationProfiles/#{identificationProfileId}/enroll")
 uri.query = URI.encode_www_form({
     # Request parameters
-    'shortAudio' => "{false}"
+    'shortAudio' => "{true}"
 })
 
 request = Net::HTTP::Post.new(uri.request_uri)
@@ -20,15 +21,20 @@ request['Content-Type'] = 'multipart/form-data'
 # Request headers
 request['Ocp-Apim-Subscription-Key'] = ENV['Subscription_Key']
 # Request body
-request.body = File.read('public/ijlal.wav')
+request.body = File.read('public/003.wav')
 
 response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
     http.request(request)
 end
-
+# r = JSON.parse(response.header)
+# puts r
 puts response.body
-puts response.header
-
+puts response["content-type"]
+puts response.code
+puts response["operation-location"]
+# response.each_header do |key, value|
+#     p "#{key} => #{value}"
+# end
 
 end
 
