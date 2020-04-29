bc1252a8d59673275de1bbd440392ce042b049db
Ijlal Gondal <ijlal.gondal@ontariotechu.net>
2020-04-29 12:09:28 -0400

    testing

--- lib/speaker_identification/identification_api.rb
+++ lib/speaker_identification/identification_api.rb
@@ -7,12 +7,12 @@ module SpeakerIdentification
         puts "beginning test"
     
     
-identificationProfileId = "{35f500b0-40c1-4160-bf83-cf2a7f7e49e7,111f427c-3791-468f-b709-fcef7660fff9}"
+identificationProfileId = "{35f500b0-40c1-4160-bf83-cf2a7f7e49e7,492159e5-2562-48c0-a354-481ef6341f8d}"
 
-uri = URI('https://westus.api.cognitive.microsoft.com/spid/v1.0/identify?identificationProfileIds={35f500b0-40c1-4160-bf83-cf2a7f7e49e7,111f427c-3791-468f-b709-fcef7660fff9}')
+uri = URI('https://westus.api.cognitive.microsoft.com/spid/v1.0/identify?identificationProfileIds={35f500b0-40c1-4160-bf83-cf2a7f7e49e7,492159e5-2562-48c0-a354-481ef6341f8d}')
 uri.query = URI.encode_www_form({
     # Request parameters
-    'shortAudio' => "{false}"
+    'shortAudio' => "{true}"
 })
 
 request = Net::HTTP::Post.new(uri.request_uri)
@@ -21,13 +21,14 @@ request['Content-Type'] = 'application/octet-stream'
 # Request headers
 request['Ocp-Apim-Subscription-Key'] = ENV['Subscription_Key']
 # Request body
-request.body = File.read('public/ijlal.wav')
+request.body = File.read('public/003.wav')
 
 response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
     http.request(request)
 end
 
 puts response.body
+puts response.header
 
 end
 
